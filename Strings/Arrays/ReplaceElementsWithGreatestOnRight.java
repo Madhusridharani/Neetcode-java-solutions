6d3c5840ee9d7cd82a9a22185b8d61cfc0e375c5
Madhu Sri <madhusridharan1805@gmail.com>
2026-03-12 06:23:00 +0530

    Update ReplaceElementsWithGreatestOnRight.java

--- Strings/Arrays/ReplaceElementsWithGreatestOnRight.java
+++ Strings/Arrays/ReplaceElementsWithGreatestOnRight.java
@@ -1,4 +1,6 @@
-class Solution {
+// Problem: Replace Elements With Greatest Element On Right Side
+// Platform: NeetCode
+// Difficulty: Easyclass Solution {
     public int[] replaceElements(int[] arr) {
         int max = -1;
         for (int i = arr.length - 1; i >= 0; i--) {
